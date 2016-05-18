97f4181e89c018ac35f324a7905d4c0348e587c7
Cosmin Mihai VACARESCU <sinmax_mv@yahoo.com>
2016-05-18 18:04:55 +0000

    working single player game

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,29 @@
 /* globals Phaser:false */
-// create BasicGame Class
-BasicGame = {
 
+//  Generic global variables
+var game, myDataRef;
+var platforms, stars;
+var player, cursors;
+var score = 0;
+var scoreText;
+
+window.onload = function() {
+    /* globals Phaser:false, BasicGame: false */
+    //  Create your Phaser game and inject it into the game div.
+    //  We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
+    //  We're using a game size of 800 x 6000 here, but you can use whatever you feel makes sense for your game of course.
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+    
+    // Adding main state & start the game.
+    game.state.add("PlayGame", PlayGame);
+    game.state.start("PlayGame");
 };
 
 // create Game function in BasicGame
-BasicGame.Game = function (game) {
-};
+var PlayGame = function (game) {};
 
 // set Game function prototype
-BasicGame.Game.prototype = {
+PlayGame.prototype = {
 
     init: function () {
         // set up input max pointers
@@ -50,20 +64,67 @@ BasicGame.Game.prototype = {
     },
 
     preload: function () {
-
-        // Here we load the assets required for our preloader (in this case a 
-        // background and a loading bar)
-        this.load.image('logo', 'asset/phaser.png');
+        //  Preload needed media
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     },
 
     create: function () {
-        // Add logo to the center of the stage
-        this.logo = this.add.sprite(
-            this.world.centerX, // (centerX, centerY) is the center coordination
-            this.world.centerY,
-            'logo');
-        // Set the anchor to the center of the sprite
-        this.logo.anchor.setTo(0.5, 0.5);
+        //  Connect to Firebase DB
+        var myDataRef = new Firebase('https://incandescent-fire-3223.firebaseio.com/');
+        
+        //  We're going to be using physics, so enable the Arcade Physics system
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        //  A simple background for our game
+        this.add.sprite(0, 0, 'sky');
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        platforms = this.add.group();
+        //  We will enable physics for any object that is created in this group
+        platforms.enableBody = true;
+        // Here we create the ground.
+        var ground = platforms.create(0, this.world.height - 64, 'ground');
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        ground.scale.setTo(2, 2);
+        //  This stops it from falling away when you jump on it
+        ground.body.immovable = true;
+        //  Now let's create two ledges
+        var ledge = platforms.create(400, 400, 'ground');
+        ledge.body.immovable = true;
+        ledge = platforms.create(-150, 250, 'ground');
+        ledge.body.immovable = true;
+        
+        // The player and its settings
+        player = this.add.sprite(32, this.world.height - 150, 'dude');
+        //  We need to enable physics on the player
+        this.physics.arcade.enable(player);
+        //  Player physics properties. Give the little guy a slight bounce.
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 300;
+        player.body.collideWorldBounds = true;
+        //  Our two animations, walking left and right.
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+        
+        stars = this.add.group();
+        stars.enableBody = true;
+    
+        //  Here we'll create 12 of them evenly spaced apart
+        for (var i = 0; i < 12; i++)
+        {
+            //  Create a star inside of the 'stars' group
+            var star = stars.create(i * 70, 0, 'star');
+            //  Let gravity do its thing
+            star.body.gravity.y = 6;
+            //  This just gives each star a slightly random bounce value
+            star.body.bounce.y = 0.7 + Math.random() * 0.2;
+        }
+        
+        //  Create cursors and bind to keyboard
+        cursors = this.input.keyboard.createCursorKeys();
+        
+        scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
     },
 
@@ -75,6 +136,52 @@ BasicGame.Game.prototype = {
         // this callback is only really useful if you use a ScaleMode of RESIZE 
         // and place it inside your main game state.
 
+    },
+    
+    update: function () {
+        //  Collide the player and the stars with the platforms
+        this.physics.arcade.collide(player, platforms);
+        //  Collide the stars with the platforms
+        this.physics.arcade.collide(stars, platforms);
+        //  Add action when player overlaps with star
+        this.physics.arcade.overlap(player, stars, this.collectStar, null, this);
+        
+        //  Reset the players velocity (movement)
+        player.body.velocity.x = 0;
+    
+        if (cursors.left.isDown)
+        {
+            //  Move to the left
+            player.body.velocity.x = -150
+            player.animations.play('left');
+        }
+        else if (cursors.right.isDown)
+        {
+            //  Move to the right
+            player.body.velocity.x = 150;
+            player.animations.play('right');
+        }
+        else
+        {
+            //  Stand still
+            player.animations.stop();
+            player.frame = 4;
+        }
+    
+        //  Allow the player to jump if they are touching the ground.
+        if (cursors.up.isDown && player.body.touching.down)
+        {
+            player.body.velocity.y = -350;
+        }
+    },
+    
+    collectStar: function (player, star) {
+        // Removes the star from the screen
+        star.kill();
+        
+        //  Add and update the score
+        score += 10;
+        scoreText.text = 'Score: ' + score;
     }
 
 };
